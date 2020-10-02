cfc430eda52b3f8f5811e0ec7a2249bd09a71b54
gaum123 <gorsel@roc-dev.nl>
2020-10-02 16:27:31 +0200

    H11.8 AF

--- src/H116.java
+++ src/H116.java
@@ -2,18 +2,19 @@ import java.awt.*;
 import java.applet.*;
 
 
-public class H115 extends Applet {
+public class H116 extends Applet {
 
-    public void init() {}
+    public void init() {
+    }
 
     public void paint(Graphics g) {
         int teller;
         int hoogte = 0;
         int breedte = 0;
-        int x = 300;
-        int y = 300;
+        int x = 120;
+        int y = 120;
 
-        for(teller = 0; teller <= 50; teller++) {
+        for (teller = 0; teller <= 5; teller++) {
             hoogte += 10;
             breedte += 10;
             x -= 5;
--- src/H117.java
+++ src/H117.java
@@ -2,19 +2,18 @@ import java.awt.*;
 import java.applet.*;
 
 
-public class H114 extends Applet {
+public class H117 extends Applet {
 
-    public void init() {
-    }
+    public void init() {}
 
     public void paint(Graphics g) {
         int teller;
         int hoogte = 0;
         int breedte = 0;
-        int x = 120;
-        int y = 120;
+        int x = 300;
+        int y = 300;
 
-        for (teller = 0; teller <= 5; teller++) {
+        for(teller = 0; teller <= 50; teller++) {
             hoogte += 10;
             breedte += 10;
             x -= 5;
