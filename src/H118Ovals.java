213eb0cc3a7fc522f671e30a4436f9ec010dda83
gaum123 <gorsel@roc-dev.nl>
2020-10-02 16:35:09 +0200

    H11.8 AF

--- src/H118Ovals.java
+++ src/H118Ovals.java
@@ -1,2 +1,21 @@
-public class H118Ovals {
-}
+import java.awt.*;
+import java.applet.*;
+
+
+    public class H118Ovals extends Applet {
+
+        public void init() {
+        }
+
+        public void paint(Graphics g) {
+            int teller;
+            int hoogte = 0;
+            int breedte = 0;
+
+            for (teller = 0; teller <= 100; teller++) {
+                hoogte += 10;
+                breedte += 10;
+                g.drawOval(20, 20, breedte, hoogte);
+            }
+        }
+    }
