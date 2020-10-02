57294744843ddb82a6ac3f78c4b7a9ef3c259d81
gaum123 <gorsel@roc-dev.nl>
2020-10-02 16:22:26 +0200

    H11.5 AF

---
+++ src/H115.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.applet.*;
+
+
+public class H115 extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int teller;
+        int hoogte = 0;
+        int breedte = 0;
+        int x = 300;
+        int y = 300;
+
+        for(teller = 0; teller <= 50; teller++) {
+            hoogte += 10;
+            breedte += 10;
+            x -= 5;
+            y -= 5;
+            g.drawOval(x, y, breedte, hoogte);
+        }
+    }
+}
